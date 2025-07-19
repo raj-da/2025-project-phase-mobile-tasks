5ce9a4951199902e78022f1ef98694c6f949cd8c
Rajaf Dereje <rajafgudeta@gmail.com>
2025-07-19 22:46:23 +0300

    add circlIcon method

--- mobile/Rajaf_Dereje/ecommerce_ui_clone/lib/widgets/Icons.dart
+++ mobile/Rajaf_Dereje/ecommerce_ui_clone/lib/widgets/Icons.dart
@@ -19,3 +19,26 @@ Widget squareIcon({
     ),
   );
 }
+
+
+Widget circleIcon({
+  required IconData icon,
+  Color iconColor = Colors.grey,
+  Color borderColor = Colors.grey,
+  Color backGroundColor = Colors.white,
+  double size = 24,
+}) {
+  return Container(
+    decoration: BoxDecoration(
+      border: Border.all(style: BorderStyle.solid, color: borderColor),
+      borderRadius: BorderRadius.circular(100),
+      color: backGroundColor,
+    ),
+    child: IconButton(
+      onPressed: () {
+        debugPrint("Back arrow Icon");
+      },
+      icon: Icon(icon, color: iconColor, size: size),
+    ),
+  );
+}
