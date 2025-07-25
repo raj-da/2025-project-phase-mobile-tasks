1de279c5438e9b9c27c948eeee108babd7dfa6a0
Rajaf Dereje <rajafgudeta@gmail.com>
2025-07-25 12:06:26 +0300

    add repository(interface) for product feature

--- mobile/Rajaf_Dereje/ecommerce_ui_clone/lib/features/product/domain/usecases/create_product_usecase.dart
+++ mobile/Rajaf_Dereje/ecommerce_ui_clone/lib/features/product/domain/usecases/create_product_usecase.dart
@@ -1,3 +1,12 @@
+import '../entities/product.dart';
+import '../repositories/product_repository.dart';
+
 class CreateProductUsecase {
-  
+  final ProductRepository repository;
+
+  CreateProductUsecase(this.repository);
+
+  Future<void> call(Product product) async {
+    await repository.createproduct(product);
+  }
 }
