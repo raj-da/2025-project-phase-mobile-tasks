c6c74bd1ba1727841ad84ecfdf85ca56121a5ad0
Rajaf Dereje <rajafgudeta@gmail.com>
2025-07-29 21:32:39 +0300

    add networkInfo class implementation

---
+++ mobile/Rajaf_Dereje/ecommerce_ui_clone/lib/core/network/network_info.dart
@@ -0,0 +1,16 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+/// To check if our device is online
+///
+abstract class NetworkInfo {
+  Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetworkInfo {
+  final InternetConnectionChecker connectionChecker;
+
+  NetworkInfoImpl(this.connectionChecker);
+
+  @override
+  Future<bool> get isConnected => connectionChecker.hasConnection;
+}
--- mobile/Rajaf_Dereje/ecommerce_ui_clone/test/features/product/data/repositories/product_repository_impl_test.mocks.dart
+++ mobile/Rajaf_Dereje/ecommerce_ui_clone/test/features/product/data/repositories/product_repository_impl_test.mocks.dart
@@ -5,7 +5,7 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'dart:async' as _i4;
 
-import 'package:ecommerce_ui_clone/core/platform/network_info.dart' as _i6;
+import 'package:ecommerce_ui_clone/core/network/network_info.dart' as _i6;
 import 'package:ecommerce_ui_clone/features/product/data/datasources/product_local_data_source.dart'
     as _i5;
 import 'package:ecommerce_ui_clone/features/product/data/datasources/product_remote_data_source.dart'
