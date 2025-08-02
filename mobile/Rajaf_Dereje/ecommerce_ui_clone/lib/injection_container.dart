ee69df2ca9aee962997c83568cbac6508dc19096
Rajaf Dereje <rajafgudeta@gmail.com>
2025-08-02 18:46:03 +0300

    add dependency injection

---
+++ mobile/Rajaf_Dereje/ecommerce_ui_clone/lib/injection_container.dart
@@ -0,0 +1,67 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'core/network/network_info.dart';
+import 'features/product/data/datasources/product_local_data_source.dart';
+import 'features/product/data/datasources/product_local_data_source_impl.dart';
+import 'features/product/data/datasources/product_remote_data_source.dart';
+import 'features/product/data/datasources/product_remote_data_source_impl.dart';
+import 'features/product/data/repositories/product_repository_impl.dart';
+import 'features/product/domain/repositories/product_repository.dart';
+import 'features/product/domain/usecases/create_product_usecase.dart';
+import 'features/product/domain/usecases/delete_product_usecase.dart';
+import 'features/product/domain/usecases/update_product_usecase.dart';
+import 'features/product/domain/usecases/view_product_usecase.dart';
+import 'features/product/presentation/bloc/product_bloc.dart';
+
+final sl = GetIt.instance;
+
+void init() async {
+  //! Features - Product
+  // Bloc
+  sl.registerFactory(
+    () => ProductBloc(
+      viewAllProductsUsecase: sl(),
+      viewProductUsecase: sl(),
+      createProductUsecase: sl(),
+      updateProductUsecase: sl(),
+      deleteProductUsecase: sl(),
+    ),
+  );
+
+  // Use cases
+  sl.registerLazySingleton(() => CreateProductUsecase(sl()));
+  sl.registerLazySingleton(() => DeleteProductUsecase(sl()));
+  sl.registerLazySingleton(() => UpdateProductUsecase(sl()));
+  sl.registerLazySingleton(() => ViewProductUsecase(sl()));
+  sl.registerLazySingleton(() => ViewProductUsecase(sl()));
+
+  // Repository
+  sl.registerLazySingleton<ProductRepository>(
+    () => ProductRepositoryImpl(
+      remoteDataSource: sl(),
+      localDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+
+  // Data sources
+  sl.registerLazySingleton<ProductRemoteDataSource>(
+    () => ProductRemoteDataSourceImpl(dio: sl()),
+  );
+  sl.registerLazySingleton<ProductLocalDataSource>(
+    () => ProductLocalDataSourceImpl(sharedPreferences: sl()),
+  );
+
+  //! Core
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+
+  //! External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+
+  sl.registerLazySingleton(() => Dio());
+  sl.registerLazySingleton(() => InternetConnectionChecker.createInstance());
+}
