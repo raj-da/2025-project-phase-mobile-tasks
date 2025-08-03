e8e4b82ae04aaacf18d71012ae8adbaa35eda980
Rajaf Dereje <rajafgudeta@gmail.com>
2025-08-03 15:37:45 +0300

    add equatable to failuer class

--- mobile/Rajaf_Dereje/ecommerce_ui_clone/lib/core/error/failures.dart
+++ mobile/Rajaf_Dereje/ecommerce_ui_clone/lib/core/error/failures.dart
@@ -1,16 +1,26 @@
-abstract class Failure {
+import 'package:equatable/equatable.dart';
+
+String generalFailureMessege = 'Failure';
+String generalServerFailureMessege = 'Server Failure';
+String generalCachedFailureMessege = 'Cache Failure';
+String generalNetworkFailureMessege = 'Network Failure';
+
+abstract class Failure extends Equatable {
   final String messege;
-  Failure(this.messege);
+  const Failure({this.messege = 'Failure'});
+
+  @override
+  List<Object> get props => [messege];
 }
 
 class ServerFailure extends Failure {
-  ServerFailure(super.messege);
+  const ServerFailure({super.messege = 'Server Failure'});
 }
 
 class CachedFailure extends Failure {
-  CachedFailure(super.messege);
+  const CachedFailure({super.messege = 'Cache Failure'});
 }
 
 class NetworkFailure extends Failure {
-  NetworkFailure(super.messege);
+  const NetworkFailure({super.messege = 'Network Failure'});
 }
