ab85ccdce8b107c189e750cc9474fd4d5d7a63fd
Rajaf Dereje <rajafgudeta@gmail.com>
2025-08-03 15:40:12 +0300

    add product_model_test

---
+++ mobile/Rajaf_Dereje/ecommerce_ui_clone/test/features/product/data/models/product_model_test.dart
@@ -0,0 +1,44 @@
+import 'package:ecommerce_ui_clone/features/product/data/models/product_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  //* dummy data to test toJson method
+  const tproductModel = ProductModel(
+    id: '1',
+    name: 'Test Product',
+    description: 'A test product',
+    price: '99',
+    imageUrl: 'test.png',
+  );
+
+  //* dummy data to test fromJson constructor
+  final tproductJson = {
+    'id': '1',
+    'name': 'Test Product',
+    'description': 'A test product',
+    'price': '99',
+    'imageUrl': 'test.png',
+  };
+
+  group('Product model', () {
+    test(
+      '''should return a valid ProductModel
+     when fromJson is called with map<String, dynamic>''',
+      () async {
+        // Act
+        final result = ProductModel.fromJson(tproductJson);
+
+        // Assert
+        expect(result, equals(tproductModel));
+      },
+    );
+
+    test('toJson should return a valid map', () async {
+      // Act
+      final result = tproductModel.toJson();
+
+      // Assert
+      expect(result, equals(tproductJson));
+    });
+  }); // product model group
+}
